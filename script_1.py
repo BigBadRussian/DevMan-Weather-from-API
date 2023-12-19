23128b91f4b0319856aad6f6fea58feb95e895f0
BigBadRussian <skripkoae@gmail.com>
2023-12-19 14:51:29 +0400

    Done

---
+++ script_1.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def request_for_weather(undefined_location):
+    url = 'https://wttr.in/'
+    http_params = {"mMnqT": "", "lang": "ru"}
+    try:
+        response = requests.get(f"{url} {geo_point}", timeout=5, params=http_params)
+        response.raise_for_status()
+        return response.text
+    except requests.ConnectionError:
+        print('Connection Error')
+    except requests.HTTPError:
+        print('HTTP error')
+    except requests.Timeout:
+        print('Timeout occurred')
+
+
+if __name__ == '__main__':
+    geo_points = ['London', 'svo', 'Череповец']
+    for geo_point in geo_points:
+        print(request_for_weather(geo_point))
