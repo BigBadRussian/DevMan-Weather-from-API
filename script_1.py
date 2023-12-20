458f2a618cdbf92752be92837280bd03c764bc99
BigBadRussian <skripkoae@gmail.com>
2023-12-20 19:38:11 +0400

    url больше не является аргументов функции request, теперь работает как переменная внутри данной функции

--- script_1.py
+++ script_1.py
@@ -1,7 +1,8 @@
 import requests
 
 
-def request_for_weather(url, geo_point, http_params):
+def request_for_weather(geo_point, http_params):
+    url = 'https://wttr.in/'
     response = requests.get(f"{url} {geo_point}", timeout=5, params=http_params)
     response.raise_for_status()
     return response.text
@@ -11,7 +12,7 @@ def main():
     geo_points = ['London', 'svo', 'Череповец']
     for geo_point in geo_points:
         try:
-            print(request_for_weather('https://wttr.in/', geo_point, {"mMnqT": "", "lang": "ru"}))
+            print(request_for_weather(geo_point, {"mMnqT": "", "lang": "ru"}))
         except requests.HTTPError:
             print("HTTP Error")
         except requests.Timeout:
