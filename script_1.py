b1773162739693330331a04bef3153c32543bded
BigBadRussian <skripkoae@gmail.com>
2023-12-20 14:23:43 +0400

    Добавлена функция main. Изменена функция запроса

--- script_1.py
+++ script_1.py
@@ -4,22 +4,22 @@ import requests
 def request_for_weather(geo_point):
     url = 'https://wttr.in/'
     http_params = {"mMnqT": "", "lang": "ru"}
-    try:
-        response = requests.get(f"{url} {geo_point}", timeout=5, params=http_params)
-        response.raise_for_status()
-        return response.text
-    except requests.ConnectionError:
-        print('Connection Error')
-    except requests.HTTPError:
-        print('HTTP error')
-    except requests.Timeout:
-        print('Timeout occurred')
+    response = requests.get(f"{url} {geo_point}", timeout=5, params=http_params)
+    response.raise_for_status()
+    return response.text
 
 
 def main():
-    geo_points = ['London', 'svo', 'Череповец']
+    geo_points = ['London', 'London', 'Череповец']
     for geo_point in geo_points:
-        print(request_for_weather(geo_point))
+        try:
+            print(request_for_weather(geo_point))
+        except requests.HTTPError:
+            print("HTTP Error")
+        except requests.Timeout:
+            print("Timeout occurred")
+        except requests.ConnectionError:
+            print("Connection Error")
 
 
 if __name__ == '__main__':
